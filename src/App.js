eb8b35af2cb39c6bd3f5d49f9e9726955d97bcfe
Siddharaj Yadav <sidharajyadav157@gmail.com>
2024-10-17 19:41:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import TopRatedPage from "./pages/TopRatedPage";
+import UpcomingPage from "./pages/UpcomingPage";
+import SingleMoviePage from "./pages/SingleMoviePage";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/top-rated" component={TopRatedPage} />
+          <Route path="/upcoming" component={UpcomingPage} />
+          <Route path="/movie/:id" component={SingleMoviePage} />
+        </Switch>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
