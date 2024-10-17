2ca5feb5179bdf8a4ed2a4e727f57ae302540c23
Siddharaj Yadav <sidharajyadav157@gmail.com>
2024-10-17 19:43:59 +0530

    reduxcommit

---
+++ src/redux/reducers.js
@@ -0,0 +1,27 @@
+import { combineReducers } from "redux";
+
+const initialState = {
+  popularMovies: [],
+  topRatedMovies: [],
+  upcomingMovies: [],
+  movieDetail: {},
+};
+
+const movieReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SET_POPULAR_MOVIES":
+      return { ...state, popularMovies: action.payload };
+    case "SET_TOP_RATED_MOVIES":
+      return { ...state, topRatedMovies: action.payload };
+    case "SET_UPCOMING_MOVIES":
+      return { ...state, upcomingMovies: action.payload };
+    case "SET_MOVIE_DETAIL":
+      return { ...state, movieDetail: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  movie: movieReducer,
+});
